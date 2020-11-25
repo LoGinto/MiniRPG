74c4b9c1acf360aafababd8f55b556f1c3ac64ea
LoGinto <46216287+LoGinto@users.noreply.github.com>
2020-11-25 23:49:24 +0400

    Inventory setup

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SoulItem;
+
+namespace RPG.UI { 
+public class UIManager : MonoBehaviour
+{
+        public GameObject selectWindow;
+        public GameObject hudUI;
+        public GameObject weaponSlotPrefab;
+        public GameObject weaponInventoryWindow;
+        bool inventoryOpen = false;
+        public Transform weaponSlotParent;
+        WeaponInventorySlot[] weaponSlots;
+        public PlayerInventory inventoryScript;
+
+        private void Start()
+        {
+            weaponSlots = weaponSlotParent.GetComponentsInChildren<WeaponInventorySlot>();
+        }
+        private void Update()
+        {
+            HandleInventoryInput();
+        }
+        void CloseAllInventoryWindows()
+        {
+            weaponInventoryWindow.SetActive(false);
+        }
+        public void UpdateUI()
+        {
+            for(int i = 0; i < weaponSlots.Length; i++)
+            {
+                if (i < inventoryScript.weaponsInventory.Count)
+                {
+                    if(weaponSlots.Length < inventoryScript.weaponsInventory.Count)
+                    {
+                        Instantiate(weaponSlotPrefab,weaponSlotParent);
+                        weaponSlots = weaponSlotParent.GetComponentsInChildren<WeaponInventorySlot>();
+                    }
+                    weaponSlots[i].AddItem(inventoryScript.weaponsInventory[i]);
+                }
+                else
+                {
+                    weaponSlots[i].ClearInventorySlot();
+                }
+            }
+        }
+        void HandleInventoryInput()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                inventoryOpen = !inventoryOpen;
+            }
+            if (inventoryOpen == true)
+            {
+                UpdateUI();
+                hudUI.SetActive(false);
+            }
+            else
+            {
+                CloseAllInventoryWindows();
+                hudUI.SetActive(true);
+            }
+            selectWindow.SetActive(inventoryOpen);
+        }
+}
+}
---
+++ Assets/Scripts/Weapon/WeaponInventorySlot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SoulItem;
+using UnityEngine.UI;
+namespace RPG.UI
+{
+    public class WeaponInventorySlot : MonoBehaviour
+    {
+        public Image icon;
+        WeaponItem item;
+
+        public void AddItem(WeaponItem newItem)
+        {
+            item = newItem;
+            icon.sprite = item.itemIcon;
+            icon.gameObject.SetActive(true);
+            gameObject.SetActive(true);
+        }
+        public void ClearInventorySlot()
+        {
+            item = null;
+            icon.sprite = null;
+            icon.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+        }
+    }
+}
